b7e9b5f7414955ef564c8e175f03f98c4e998900
Dgley Ramos <dgleysilva@gmail.com>
2022-09-28 12:21:44 -0300

    Adicionando segurança com o spring security

--- src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
+++ src/main/java/one/digitalinnovation/parking/config/SwaggerConfig.java
@@ -1,12 +1,21 @@
 package one.digitalinnovation.parking.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,6 +30,26 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("one.digitalinnovation.parking"))
                 .build()
                 .apiInfo(metaData());
+    }
+
+    private SecurityContext actuatorSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+
+    private List<SecurityScheme> basicScheme() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        schemeList.add(new BasicAuth("basicAuth"));
+        return schemeList;
     }
 
     private ApiInfo metaData() {
