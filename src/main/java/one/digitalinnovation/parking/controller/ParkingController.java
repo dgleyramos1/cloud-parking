c44822e86fb841649c85135968cc8fa2c44293be
Dgley Ramos <dgleysilva@gmail.com>
2022-09-28 04:06:48 -0300

    Criando mapper e dto para a class parking

--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -1,12 +1,13 @@
 package one.digitalinnovation.parking.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import one.digitalinnovation.parking.controller.dtos.ParkingDTO;
+import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinnovation.parking.model.Parking;
 import one.digitalinnovation.parking.service.ParkingService;
 
@@ -16,13 +17,18 @@ public class ParkingController {
 
     private final ParkingService parkingService;
 
-    public ParkingController(ParkingService parkingService) {
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingmapper) {
         this.parkingService = parkingService;
+        this.parkingMapper = parkingmapper;
     }
 
     @GetMapping
-    public List<Parking> findAll() {
-        return parkingService.findAll();
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
     }
 
 }
